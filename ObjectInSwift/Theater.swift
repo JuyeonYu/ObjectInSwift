4f40fbd752de8bd62ed25986f6d08cccc921ce37
Juyeon <remake867@gmail.com>
2022-01-25 23:29:26 +0900

    TicketSeller 캡슐화

--- ObjectInSwift/Theater.swift
+++ ObjectInSwift/Theater.swift
@@ -15,14 +15,6 @@ class Theater {
   }
   
   func enter(audience: Audience) {
-    if audience.bag.hasInvitation {
-      let ticket: Ticket = self.tickSeller.ticketOffice.tickets.removeFirst()
-      audience.bag.ticket = ticket
-    } else {
-      let ticket: Ticket = self.tickSeller.ticketOffice.tickets.removeFirst()
-      audience.bag.minusAmount(amount: ticket.fee)
-      tickSeller.ticketOffice.plusAmount(amount: ticket.fee)
-      audience.bag.ticket = ticket
-    }
+    tickSeller.sell(to: audience)
   }
 }
--- ObjectInSwift/TicketSeller.swift
+++ ObjectInSwift/TicketSeller.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 class TicketSeller {
-  let ticketOffice: TicketOffice
+  private let ticketOffice: TicketOffice
+  
   init(ticketOffice: TicketOffice) {
     self.ticketOffice = ticketOffice
+  }
+  
+  func sell(to audience: Audience) {
+    if audience.bag.hasInvitation {
+      let ticket: Ticket = ticketOffice.tickets.removeFirst()
+      audience.bag.ticket = ticket
+    } else {
+      let ticket: Ticket = ticketOffice.tickets.removeFirst()
+      audience.bag.minusAmount(amount: ticket.fee)
+      ticketOffice.plusAmount(amount: ticket.fee)
+      audience.bag.ticket = ticket
+    }
   }
 }
